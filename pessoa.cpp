431c092d477c05fa2fe7988e0420aaf93a934974
Guilherme Brotto <102331472+guilherme751@users.noreply.github.com>
2023-02-02 14:39:31 -0300

    calcula idade feito

--- pessoa.cpp
+++ pessoa.cpp
@@ -4,24 +4,40 @@
 
 using namespace std;
 
-static int calculaIdade(Date &dataDeNascimento, Date &dataAtual) {
+// 10/10/2002
+// 01/02/2023
+//
 
+static int calculaIdade(Date &dataDeNascimento, Date &dataAtual)
+{
+    int age = dataAtual.getAno() - dataDeNascimento.getAno();
+    if (dataAtual.getMes() > dataDeNascimento.getMes())    
+        return age;    
+    else if (dataAtual.getMes() < dataDeNascimento.getMes())    
+        return --age;    
+    else
+    {
+        if (dataAtual.getDia() >= dataDeNascimento.getDia())
+            return age;
+        else
+            return --age;
+    }
 }
 
 Pessoa::Pessoa(string nome, string genero, Date &dataDeNascimento, Date &dataAtual)
-{    
+{
     this->nome = nome;
     this->genero = genero;
     this->idade = calculaIdade(dataDeNascimento, dataAtual);
 }
-
-Pessoa::~Pessoa()
+string Pessoa::getNome() const
 {
-}
-
-string Pessoa::getNome() const {
     return this->nome;
 }
-string Pessoa::getGenero() const {
+string Pessoa::getGenero() const
+{
     return this->genero;
+}
+Pessoa::~Pessoa()
+{
 }
